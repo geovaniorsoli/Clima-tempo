f793e3a62bd14a8b89b97d2fb27a12429e103fa3
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-11-20 18:45:44 -0300

    visual + tela responsiva

--- js/script.js
+++ js/script.js
@@ -31,15 +31,15 @@ function climate(e) {
             containterClima.appendChild(local);
             
             const sense = document.createElement("h2")
-            sense.textContent = `Sensação Térmica: ${weatherData.feelslike_c} °C`
+            sense.innerHTML = `<div class="mt-3"> </div> Sensação Térmica ${weatherData.feelslike_c} °C`
             containterClima.appendChild(sense)
 
             const condicao = document.createElement('h2')
-            condicao.textContent = `Condição: ${weatherData.condition.text}`
+            condicao.innerHTML = `<div class="mt-3"> </div> <ion-icon name="partly-sunny-outline"></ion-icon> ${weatherData.condition.text}`
             containterClima.appendChild(condicao)
 
             const umidade = document.createElement('h2')
-            umidade.textContent = `Umidade: ${weatherData.humidity}%`
+            umidade.innerHTML = `<div class="mt-3"> </div> <ion-icon class="icondisplay" name="water-outline"></ion-icon> ${weatherData.humidity}%`
             containterClima.appendChild(umidade)
         }
 
@@ -67,14 +67,17 @@ window.onload = () =>{
 function styleclima(){
     const atual = new Date().getHours()
     const DOM = document.body
+    let CSS
 
-    if(atual >= 6 && atual < 12){
-        DOM.className = "manha"
-    } else if (atual >= 12 && atual < 18){
-        DOM.className = "tarde"
-        }else{ 
+    if(atual >= 6 && atual < 18){
+        DOM.className = "dia"
+        CSS = '/style/dia.css'
+    } else{ 
         DOM.className = "noite"
+        CSS = '/style/noite.css'
     }
+
+    document.getElementById('tema').setAttribute("href", CSS)
     console.log(styleclima)
 }
 
